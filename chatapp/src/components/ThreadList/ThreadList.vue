8f059b0cb6ab9bab86ae97dd371eaa524d057deb
KinuGra <techgit1024@gmail.com>
2025-07-31 14:50:39 +0900

    add: タグによるフィルター機能

--- chatapp/src/components/ThreadList/ThreadList.vue
+++ chatapp/src/components/ThreadList/ThreadList.vue
@@ -1,63 +1,50 @@
 <script setup>
-import { reactive } from 'vue';
+import { ref, reactive } from 'vue';
+import FilterModal from './FilterModal.vue';
+import { initChatData } from './dummyData.js';
 
-const chatHistory = reactive([
-    {
-        id: 0,
-        content: "こんにちは",
-        timestamp: new Date(),
-        sender: "山田",
-        type: "individual",
-    },
-    {
-        id: 1,
-        title: "数学",
-        tags: ["タグ1", "タグ2"],
-        messages: [
-            {
-                id: 1,
-                content: "数学のメモ",
-                timestamp: new Date(),
-                sender: "村田",
-            },
-            {
-                id: 2,
-                content: "青チャート",
-                timestamp: new Date(),
-                sender: "山田",
-            }
-        ],
-        type: "thread"
-    },
-    {
-        id: 3,
-        title: "英語の講義",
-        tags: ["タグ1", "タグ2"],
-        messages: [
-            {
-                id: 3,
-                content: "メモ1",
-                timestamp: new Date(),
-                sender: "村田",
-            },
-            {
-                id: 4,
-                content: "英語メモ",
-                timestamp: new Date(),
-                sender: "山田",
-            }
-        ],
-        type: "thread"
-    }
-])
+const chatHistory = reactive(initChatData)
+
+const isShowFilterModal = ref(false);
+const showModal = () => {
+    isShowFilterModal.value = true;
+}
+
+// 絞り込みようのfilterTag更新
+const handleUpdate = (tag) => {
+    filterTag.value = tag;
+    console.log(filterTag.value + "で絞り込みます")
+}
+
+const filterTag = ref("全スレッド");
 </script>
 
 <template>
-    <v-btn>絞り込み</v-btn>
+    <v-btn @click="showModal">絞り込み</v-btn>
     <v-btn>❌</v-btn>
     <p>スレッド一覧</p>
+
+    <!-- フィルターモーダル -->
+    <div v-if="isShowFilterModal === true" class="modal-wrapper">
+        <FilterModal @update="handleUpdate"/>
+    </div>
+
+    <!-- 全スレッド -->
     <div v-for="(chat, i) in chatHistory.slice().reverse()" :key="chat.id">
-        <ul v-if="chat.type === 'thread'">
+        <ul v-if="chat.type === 'thread' && filterTag === '全スレッド'">
+            <li>
+                {{ chat.title }}
+                <span v-for="(tag, i) in chat.tags" :key="chat.id">
+                    {{ tag }}
+                </span>
+                <ul><li :style="{ marginLeft: '10px' }">{{ chat.messages[0].content }}</li></ul>
+            </li>
+        </ul>
+    </div>
+
+    <!-- タグによる絞り込み -->
+    <div v-for="(chat, i) in chatHistory.slice().reverse()" :key="chat.id">
+        <ul v-if="chat.type === 'thread' && filterTag !== '全スレッド' && chat.tags.includes(filterTag)">
             <li>
                 {{ chat.title }}
                 <span v-for="(tag, i) in chat.tags" :key="chat.id">
@@ -68,3 +55,19 @@ const chatHistory = reactive([
         </ul>
     </div>
 </template>
+
+<style>
+.modal-wrapper {
+    position: fixed;
+    top: 10%;
+    left: 50%;
+    transform: translateX(-50%);
+    background-color: #ddd;
+    border: 1px solid #ccc;
+    padding: 1.5rem;
+    z-index: 1000;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2);
+    border-radius: 8px;
+    width: 30%;
+}
+</style>
